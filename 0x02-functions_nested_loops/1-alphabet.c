9c2bd0bf8f8d7807e160b4f2e2cc0c1176ec1b24
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-09-14 17:33:07 +0600

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,17 +9,9 @@ void print_alphabet(void)
 {
 	char alpha;
 
-<<<<<<< HEAD
-			for (alpha = 'a'; alpha <= 'z'; alpha++)
-					{
-				        	_putchar(alpha);
-					}
-				_putchar('\n');
-=======
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
->>>>>>> b69eb7ed15bbb490d015ea483136865583a2ef41
 }
