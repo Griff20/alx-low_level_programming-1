1fb1ff7cd84c149d2b14b68acc0a5272dfe6cf9b
254alexis <alexanderkiprop50@gmail.com>
2022-09-15 07:57:30 -0700

    adding file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#define "MAIN_H"
+#include <stdio.h>
+void positive_or_negative(int i);
+int large_number(int, int, int,);
+void print_remainig_days(int, int, int,);
+int convert_day(int, int,);
+
+#endif /*MAIN_H*/
