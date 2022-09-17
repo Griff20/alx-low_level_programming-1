24896f9034fb8b1c7d10d572e423544f7f24ba66
254alexis <alexanderkiprop50@gmail.com>
2022-09-17 02:38:27 -0700

    updates

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char l;
+char l;
 
-	l = 'a';
-	while (l <= 'z')
-	{
-		putchar(l);
-		l++;
-	}
-	putchar('\n');
-	return (0);
+l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char l;
+char l;
 
-	l = 'a';
-	while (l <= 'z')
-	{
-		putchar(l);
-		l++;
-	}
-	l = 'A';
-	while (l <= 'Z')
-	{
-		putchar(l);
-		l++;
-	}
-	putchar('\n');
-	return (0);
+l = 'a';
+while (l <= 'z')
+{
+putchar(l);
+l++;
+}
+l = 'A';
+while (l <= 'Z')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
 }
