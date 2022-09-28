f91e1a6a10a422d089f668e357ac3b6c314f5c7d
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-09-28 14:41:13 +0600

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,42 +1,40 @@
 #include "main.h"
+
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
 /**
-*_evaluate - evaluate function sqrt
-*@i: interger
-*@n: interger
-*Return: evaluate sqrt
+* is_divisible - Checks if a number is divisible.
+* @num: The number to be checked.
+* @div: The divisor.
+* Return: If the number is divisible - 0.
+*If the number is not divisible - 1.
 */
-
-int _evaluate(int i, int n)
+int is_divisible(int num, int div)
 {
-/*Evaluate function*/
-if (n == 0 || n == 1)
-return (n);
+if (num % div == 0)
+return (0);
 
-else if (i * i < n)
-return (_evaluate(i + 1, n));
+if (div == num / 2)
+return (1);
 
-else if (i * i == n) /*Condiction base*/
-return (i);
-
-return (-1);
+return (is_divisible(num, div + 1));
 }
 
 /**
-*_sqrt_recursion - evaluate sqrt
-*@n: interger
-*Return: Sqrt_recursion
+* is_prime_number - Checks if a number is prime.
+* @n: The number to be checked.
+* Return: If the integer is not prime - 0.
 */
-
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-int i = 0;
+int div = 2;
 
-if (i < 0) /*If n is negative*/
-return (-1);
+if (n <= 1)
+return (0);
 
-else
-{
-return (_evaluate(i, n)); /*Recursive call*/
-}
+if (n >= 2 && n <= 3)
+return (1);
 
+return (is_divisible(n, div));
 }
