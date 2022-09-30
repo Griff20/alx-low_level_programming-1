201e0ee8ffdcb3eecf19eb622b6dff8bc661c8b7
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-09-30 18:50:59 +0600

    Update 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* main - returns first parameter passed at command line
+* @argc: number of variables passed at command line
+* @argv: pointer to a list of pointers, that are at heads of strings
+* Return: int (zero) on success
+*/
+
+int main(int argc, char **argv)
+{
+argc = 0;
+printf("%s\n", *(argv + argc));
+exit(EXIT_SUCCESS);
+}
