7bf7eb3076217c954822eb909539d40bb01a1a58
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-10-04 22:55:42 +0600

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-* malloc_checked - allocates memory using malloc
-* @b: memory to allocate
-* Return: pointer to allocated memory or normal process termination if error
+* malloc_checked - Allocates memory using malloc.
+* @b: The number of bytes to be allocated.
+* Return: A pointer to the allocated memory.
 */
 void *malloc_checked(unsigned int b)
 {
-void *p;
-p = malloc(b);
-if (p == NULL)
+void *mem = malloc(b);
+if (mem == NULL)
 exit(98);
-return (p);
+return (mem);
 }
