3dbca85ef9aa4ff4e50bf58a286d40c755c40dcb
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-10-04 23:54:13 +0600

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,80 +1,39 @@
-#include <main.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings into newly allocated memory
- * @s1: String 1
- * @s2: String 2
- * Return: Pointer to the new array, or NULL if it fails
+ *argstostr - concatinate strings into a single one.
+ *@ac: number of arguments.
+ *@av: pointer to arguments vector.
+ *Return: pointer to string.
  */
-char *str_concat(char *s1, char *s2)
-{
-	char *cat;
-	unsigned int s1_length = 0;
-	unsigned int s2_length = 0;
-	unsigned int i, total_length = 0;
 
-	if (s1 != NULL)
-	{
-		while (s1[s1_length++])
-			continue;
-	}
-	else
-	{
-		s1_length = 1;
-		s1 = "";
-	}
-	if (s2 != NULL)
-	{
-		while (s2[s2_length++])
-			continue;
-	}
-	else
-	{
-		s2_length = 1;
-		s2 = "";
-	}
-	/* Storage for both null bytes is not required, hence -1 */
-	total_length = s1_length + s2_length - 1;
-	cat = malloc(total_length);
-	if (cat != NULL)
-	{
-		for (i = 0; i < s1_length; i++)
-			cat[i] = s1[i];
-		for (i = 0; i < s2_length; i++)
-			cat[i + s1_length - 1] = s2[i];
-	}
-	return (cat);
-}
-
-/**
- * argstostr - Concatenates all the arguments of your program
- * @ac: Argument count
- * @av: Argument values
- * Return: Concatenated strings
- */
 char *argstostr(int ac, char **av)
 {
-	int argument;
-	char *cat = NULL;
-	char *temp = NULL;
+	char *str;
+	int i, j, T_length = 0;
 
-	if (ac < 1 || av == NULL)
+	if (av == NULL || ac == 0)
 		return (NULL);
 
-	for (argument = 0; argument < ac; argument++)
-	{
-		temp = str_concat(temp, av[argument]);
-		if (cat != NULL)
-			free(cat);
-		if (temp == NULL)
-			return (NULL);
-		cat = str_concat(temp, "\n");
-		free(temp);
-		if (cat == NULL)
-			return (NULL);
-		temp = cat;
-	}
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			T_length++;
 
-	return (cat);
+	str = malloc(sizeof(char) * T_length + ac + 1);
+	if (str == NULL)
+		return (NULL);
+	T_length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[T_length] = av[i][j];
+			T_length++;
+		}
+		str[T_length] = '\n';
+		T_length++;
+	}
+	str[T_length] = '\0';
+	return (str);
 }
