7734bde8257273260341038610cb4bdf3af90131
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-11-02 14:17:07 +0600

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+* AUTHOR: Alexander kiprop.
+* DATE: 20 SEP 2022.
+* EMAIL: Alexanderkiprop50@gmail.com
+*/
 int main(void)
 {
 int myrand;
 int count;
 int total;
-
 srand(time(NULL));
 for (count = 0, total = 2772; total > 122; count++)
 {
@@ -16,6 +19,5 @@ printf("%c", myrand);
 total -= myrand;
 }
 printf("%c", total);
-
 return (0);
 }
