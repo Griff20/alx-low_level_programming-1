2578f3cfe82de687d08f0e36d24dd506a4e65f92
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-11-02 14:14:49 +0600

    Update 100-main.c

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
+* AUTHOR: Alexander kiprop.
+* DATE: 20 SEP 2022.
+* EMAIL: Alexanderkiprop50@gmail.com
 * main - check the code
-*
 * Return: Always 0.
 */
 int main(void)
 {
 int nb;
-
 nb = _atoi("98");
 printf("%d\n", nb);
 nb = _atoi("-402");
