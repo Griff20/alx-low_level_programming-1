b6188c380e82faea04364ac1a30c725528f6c2b7
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-11-02 14:51:01 +0600

    Update _putchar.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,6 +1,9 @@
 #include <unistd.h>
 
 /**
+* AUTHOR: Alexander kiprop.
+* DATE: 20 SEP 2022.
+* EMAIL: Alexanderkiprop50@gmail.com
 * _putchar - writes the character c to stdout
 * @c: The character to print
 * Return: On success 1.
