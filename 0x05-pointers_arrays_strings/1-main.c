530d755df02982924fc49df0262ac076306cf7b7
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-11-02 13:34:44 +0600

    Update 1-main.c

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -1,31 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
+* AUTHOR: Alexander kiprop.
+* DATE: 20 SEP 2022.
+* EMAIL: Alexanderkiprop50@gmail.com
 * main - check the code
 * Return: Always 0.
-
 */
-
 int main(void)
-
 {
-
 int a;
-
 int b;
-
-
-
 a = 98;
-
 b = 42;
-
 printf("a=%d, b=%d\n", a, b);
-
 swap_int(&a, &b);
-
 printf("a=%d, b=%d\n", a, b);
-
 return (0);
-
 }
