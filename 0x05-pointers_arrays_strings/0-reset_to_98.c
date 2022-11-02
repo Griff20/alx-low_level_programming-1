de640bccb8bbabaef6d8c286ca4749babbc8d31f
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-11-02 14:11:08 +0600

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
- * reset_to_98 - Set pointer param to 98.
- * @n: int type pointer
- */
+* AUTHOR: Alexander kiprop.
+* DATE: 20 SEP 2022.
+* EMAIL: Alexanderkiprop50@gmail.com
+* reset_to_98 - Set pointer param to 98.
+* @n: int type pointer
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
