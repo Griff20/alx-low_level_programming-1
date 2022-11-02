20988665fc64ee3bf00eed41dde17334ab24636c
Alexander Kiprop <102218687+254alexis@users.noreply.github.com>
2022-11-02 14:41:25 +0600

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
 /**
+* AUTHOR: Alexander kiprop.
+* DATE: 20 SEP 2022.
+* EMAIL: Alexanderkiprop50@gmail.com
 * _strlen - return the length of the string
 * @s: char type pointer
 * Return: length of string
